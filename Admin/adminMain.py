7c3b492c338badc1a2bcd0a4ca12951eccedf7da
Ismael Antonio Davila Rodriguez <tonyre4@gmail.com>
2017-10-29 23:44:36 -0600

    Primeros pasos

--- Admin/adminMain.py
+++ Admin/adminMain.py
@@ -1,2 +1,36 @@
-#Main file of admin main
-print "main"
+import os
+
+if os.name == 'nt':
+    import gi
+    gi.require_version("Gtk", "3.0")
+    from gi.repository import Gtk as gtk
+else:
+    import pygtk
+    pygtk.require("2.0")
+    import gtk
+
+class APP:
+
+
+    def __init__(self):
+        #Constantes
+        self.widList = []
+
+        topwin = gtk.Window()
+        topwin.connect('destroy',gtk.main_quit)
+        
+        label = gtk.Label('Administrador')
+        topwin.add(label)
+
+        self.widList.append(topwin)
+        self.widList.append(label)
+        self.showInit()
+
+        gtk.main()
+
+    def showInit(self):
+        for w in self.widList:
+            w.show()
+        del self.widList
+        
+a = APP()
--- basic_tests/pyGTKtry.py
+++ basic_tests/pyGTKtry.py
@@ -4,9 +4,13 @@ if os.name == 'nt':
     import gi
     gi.require_version("Gtk", "3.0")
     from gi.repository import Gtk as gtk
+else:
+    import pygtk
+    pygtk.require("2.0")
+    import gtk
 
 def create_window():
-    window = gtk.Window()
+    window = gtk.Window(gtk.WINDOW_TOPLEVEL)
     window.set_default_size(200, 200)
     window.connect('destroy', gtk.main_quit)
 
